b65bf0cae5b27c2b00f3f0b32aa5e4d7a828ebcf
Godfrey Lemi <godfreylemi@gmail.com>
2018-07-27 00:22:27 +0100

    move alert script
    from html file to script file

--- form.js
+++ form.js
@@ -1,6 +1,6 @@
 var i = 0; /* Set Global Variable i */
-function increment(){
-i += 1; /* Function for automatic increment of field's "Name" attribute. */
+function increment() {
+    i += 1; /* Function for automatic increment of field's "Name" attribute. */
 }
 /*
 ---------------------------------------------
@@ -9,19 +9,19 @@ Function to Remove Form Elements Dynamically
 ---------------------------------------------
 
 */
-function removeElement(parentDiv, childDiv){
-if (childDiv == parentDiv){
-alert("The parent div cannot be removed.");
-}
-else if (document.getElementById(childDiv)){
-var child = document.getElementById(childDiv);
-var parent = document.getElementById(parentDiv);
-parent.removeChild(child);
-}
-else{
-alert("Child div has already been removed or does not exist.");
-return false;
-}
+function removeElement(parentDiv, childDiv) {
+    if (childDiv == parentDiv) {
+        alert("The parent div cannot be removed.");
+    }
+    else if (document.getElementById(childDiv)) {
+        var child = document.getElementById(childDiv);
+        var parent = document.getElementById(parentDiv);
+        parent.removeChild(child);
+    }
+    else {
+        alert("Child div has already been removed or does not exist.");
+        return false;
+    }
 }
 /*
 ----------------------------------------------------------------------------
@@ -30,17 +30,17 @@ Functions that will be called upon, when user click on the Name text field.
 
 ----------------------------------------------------------------------------
 */
-function nameFunction(){
-var r = document.createElement('span');
-var y = document.createElement("INPUT");
-y.setAttribute("type", "text");
-y.setAttribute("placeholder", "Full Name");
-increment();
-y.setAttribute("Name", "textelement_" + i);
-r.appendChild(y);
-r.setAttribute("id", "id_" + i);
-document.getElementById("myForm").appendChild(r);
-document.getElementById("namebtn").disabled = true;
+function nameFunction() {
+    var r = document.createElement('span');
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Full Name");
+    increment();
+    y.setAttribute("Name", "textelement_" + i);
+    r.appendChild(y);
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("myForm").appendChild(r);
+    document.getElementById("namebtn").disabled = true;
 }
 /*
 -----------------------------------------------------------------------------
@@ -49,17 +49,17 @@ Functions that will be called upon, when user click on the E-mail text field.
 
 ------------------------------------------------------------------------------
 */
-function emailFunction(){
-var r = document.createElement('span');
-var y = document.createElement("INPUT");
-y.setAttribute("type", "text");
-y.setAttribute("placeholder", "Email Adress");
-increment();
-y.setAttribute("Name", "textelement_" + i);
-r.appendChild(y);
-r.setAttribute("id", "id_" + i);
-document.getElementById("myForm").appendChild(r);
-document.getElementById("emailbtn").disabled = true;
+function emailFunction() {
+    var r = document.createElement('span');
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Email Adress");
+    increment();
+    y.setAttribute("Name", "textelement_" + i);
+    r.appendChild(y);
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("myForm").appendChild(r);
+    document.getElementById("emailbtn").disabled = true;
 }
 /*
 -----------------------------------------------------------------------------
@@ -68,17 +68,17 @@ Functions that will be called upon, when user click on the Contact text field.
 
 ------------------------------------------------------------------------------
 */
-function contactFunction(){
-var r = document.createElement('span');
-var y = document.createElement("INPUT");
-y.setAttribute("type", "text");
-y.setAttribute("placeholder", "Phone Number");
-increment();
-y.setAttribute("Name", "textelement_" + i);
-r.appendChild(y);
-r.setAttribute("id", "id_" + i);
-document.getElementById("myForm").appendChild(r);
-document.getElementById("contactbtn").disabled = true;
+function contactFunction() {
+    var r = document.createElement('span');
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "text");
+    y.setAttribute("placeholder", "Phone Number");
+    increment();
+    y.setAttribute("Name", "textelement_" + i);
+    r.appendChild(y);
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("myForm").appendChild(r);
+    document.getElementById("contactbtn").disabled = true;
 }
 /*
 -----------------------------------------------------------------------------
@@ -87,17 +87,17 @@ Functions that will be called upon, when user click on the Message textarea fiel
 
 ------------------------------------------------------------------------------
 */
-function textareaFunction(){
-var r = document.createElement('span');
-var y = document.createElement("TEXTAREA");
-y.setAttribute("cols", "17");
-y.setAttribute("placeholder", "Your message..");
-increment();
-y.setAttribute("Name", "textelement_" + i);
-r.appendChild(y);
-r.setAttribute("id", "id_" + i);
-document.getElementById("myForm").appendChild(r);
-document.getElementById("messagebtn").disabled = true;
+function textareaFunction() {
+    var r = document.createElement('span');
+    var y = document.createElement("TEXTAREA");
+    y.setAttribute("cols", "17");
+    y.setAttribute("placeholder", "Your message..");
+    increment();
+    y.setAttribute("Name", "textelement_" + i);
+    r.appendChild(y);
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("myForm").appendChild(r);
+    document.getElementById("messagebtn").disabled = true;
 }
 /*
 -----------------------------------------------------------------------------
@@ -106,10 +106,14 @@ Functions that will be called upon, when user click on the Reset Button.
 
 ------------------------------------------------------------------------------
 */
-function resetElements(){
-document.getElementById('myForm').innerHTML = '';
-document.getElementById("namebtn").disabled = false;
-document.getElementById("emailbtn").disabled = false;
-document.getElementById("contactbtn").disabled = false;
-document.getElementById("messagebtn").disabled = false;
+function resetElements() {
+    document.getElementById('myForm').innerHTML = '';
+    document.getElementById("namebtn").disabled = false;
+    document.getElementById("emailbtn").disabled = false;
+    document.getElementById("contactbtn").disabled = false;
+    document.getElementById("messagebtn").disabled = false;
+}
+//script for to acknowledge enquiry
+function thankyou() {
+    alert("Your enquiry has been recorded");
 }
